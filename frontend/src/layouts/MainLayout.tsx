1f140c02dc76171c23a35cf07d7958dd2cb2fba6
dzelajto <jlelito@student.agh.edu.pl>
2026-05-24 23:26:16 +0200

    layouts

---
+++ frontend/src/layouts/MainLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/Navbar';
+
+export default function MainLayout() {
+  return (
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      <Navbar />
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+      <footer className="p-6 text-center text-gray-500 border-t">EduEnroll © 2026</footer>
+    </div>
+  );
+}
