f24b047cb069196f08fd28c44134af237c518d58
klarakkk <klarak@onet.pl>
2026-05-25 21:01:34 +0200

    wcag eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import jsxA11y from 'eslint-plugin-jsx-a11y' // 1. Importujemy wtyczkę WCAG
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -14,9 +15,16 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      jsxA11y.flatConfigs.recommended, // 2. Włączamy domyślne sprawdzanie WCAG
     ],
     languageOptions: {
       globals: globals.browser,
+    },
+    rules: {
+      // 3. Dodatkowe, rygorystyczne reguły dostępności dla zespołu:
+      'jsx-a11y/alt-text': 'error',       // Każdy obrazek musi mieć opis dla czytników ekranu
+      'jsx-a11y/aria-props': 'error',     // Atrybuty ARIA muszą być poprawne technicznie
+      'jsx-a11y/aria-proptypes': 'error', // Wartości w atrybutach ARIA muszą mieć odpowiedni typ
     },
   },
 ])
