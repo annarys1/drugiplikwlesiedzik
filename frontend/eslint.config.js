d13443aad3d0b05e693a94583741ea9e71ce4017
klarakkk <klarak@onet.pl>
2026-05-25 21:19:43 +0200

    dziala??????

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import jsxA11y from 'eslint-plugin-jsx-a11y' // Importujemy wtyczkę WCAG
+import jsxA11y from 'eslint-plugin-jsx-a11y'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -15,16 +15,15 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      jsxA11y.flatConfigs.recommended, // Włączamy automatyczne sprawdzanie WCAG
+      jsxA11y.flatConfigs.recommended,
     ],
     languageOptions: {
       globals: globals.browser,
     },
     rules: {
-      // Dodatkowe, rygorystyczne reguły dostępności dla zespołu:
-      'jsx-a11y/alt-text': 'error',       // Każdy obrazek musi mieć opis dla czytników
-      'jsx-a11y/aria-props': 'error',     // Atrybuty ARIA muszą być poprawne technicznie
-      'jsx-a11y/aria-proptypes': 'error', // Wartości w atrybutach ARIA muszą mieć odpowiedni typ
+      'jsx-a11y/alt-text': 'error',
+      'jsx-a11y/aria-props': 'error',
+      'jsx-a11y/aria-proptypes': 'error',
     },
   },
 ])
