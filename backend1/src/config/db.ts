46afadb63664b9996bc5142b85c58c443b2431bc
Klara <klarak@onet.pl>
2026-06-01 10:06:30 +0000

    Poprawka

--- backend1/src/config/db.ts
+++ backend1/src/config/db.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT || '3306'),
-  database: process.env.DB_NAME, // Upewnij się, że tak samo nazywa się w .env!
+  database: process.env.DB_NAME, 
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   waitForConnections: true,
@@ -25,7 +25,7 @@ async function testConnection() {
     console.log('Czas bazy:', rows[0].time);
     console.log('Wersja bazy:', rows[0].version);
   } catch (error: any) {
-    console.error('❌ Błąd połączenia z bazą Apache:', error.message);
+    console.error('❌ Błąd połączenia z bazą :', error.message);
   }
   
 }
--- backend1/src/index.ts
+++ backend1/src/index.ts
@@ -1,17 +1,34 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import pool from './config/db'; // To automatycznie wywoła nasz test połączenia z bazą
 import authRoutes from './routes/authRoutes';
+import childrenRoutes from './routes/childrenRoutes';
+import institutionRoutes from './routes/institutionRoutes';
+import applicationRoutes from './routes/applicationRoutes';
+import healthRoutes from './routes/healthRoutes';
+
+
 dotenv.config();
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:3000', // frontend
+  credentials: true
+}));
 const PORT = process.env.PORT || 5000;
 
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/children', childrenRoutes);
+app.use('/api/institution', institutionRoutes);
+app.use('/api/application', applicationRoutes);
 
-// Prosty testowy punkt dostępu (Endpoint)
+
+app.use('/api', healthRoutes);
+
+
 app.get('/', (req, res) => {
   res.send('System rekrutacji EduEnroll działa pomyślnie!');
 });
@@ -19,3 +36,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Serwer backendowy EduEnroll wystartował na porcie ${PORT}`);
 });
+
+
