58ac715bddba6674cfd1801e210908d4dcadb4cf
Klara <klarak@onet.pl>
2026-06-12 09:57:30 +0000

    commit julka

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 8802, 
+    proxy: {
+      '/api': {
+        target: 'http://149.156.194.192:8801',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
