5b6e202b69fccbc467987282eb618cf3843a6690
AnepifHub <michael1256@interia.pl>
2026-06-29 23:52:43 +0200

    fix: zachowaj trasy Profile i AddHeadmaster przy ThemeProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,8 @@ import FacilityDashboard from './pages/FacilityDashboard';
 import GminaDashboard from './pages/GminaDashboard';
 import ApplicationPage from './pages/ApplicationPage';
 import MyApplications from './pages/MyApplications';
+import Profile from './pages/Profile';
+import AddHeadmaster from './pages/AddHeadmaster';
 
 const router = createBrowserRouter([
   {
@@ -29,8 +31,10 @@ const router = createBrowserRouter([
       { path: 'rodzic', element: <ParentDashboard /> },
       { path: 'rodzic/nowy-wniosek', element: <ApplicationPage /> },
       { path: 'rodzic/moje-wnioski', element: <MyApplications /> },
+      { path: 'rodzic/profil', element: <Profile /> },
       { path: 'placowka', element: <FacilityDashboard /> },
       { path: 'gmina', element: <GminaDashboard /> },
+      { path: 'gmina/dodaj-dyrektora', element: <AddHeadmaster /> },
     ],
   },
 ]);
